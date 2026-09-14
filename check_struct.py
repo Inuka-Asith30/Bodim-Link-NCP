7b15806de2b942608f7fb890ccdaea34ed94d7d3
DarshanaSampath <rathnayakekasun00@gmail.com>
2026-09-14 22:46:48 +0530

    UI: Add static mock reviews section for presentation

---
+++ check_struct.py
@@ -0,0 +1,8 @@
+﻿import os
+
+with open('templates/boarding_details.html', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# We need to find a place to insert the reviews section.
+# Usually, before the closing </main> or closing </div> of the main content.
+# Let's see the structure of boarding_details.html
