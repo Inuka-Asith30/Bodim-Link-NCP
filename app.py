6401fe11a205bb7094253a9b0d34dacfe53ba165
DarshanaSampath <rathnayakekasun00@gmail.com>
2026-09-14 22:52:58 +0530

    Feat: Add About Us and Contact Us pages

--- app.py
+++ app.py
@@ -210,6 +210,16 @@ def login():
 # ----------------------------------------------------
 # Dashboards and Logout
 # ----------------------------------------------------
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
 @app.route('/student')
 def student_dashboard():
     
